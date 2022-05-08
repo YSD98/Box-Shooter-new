5bfa2e21b88ba55134ed16cad619cbd874f015d5
Yash <62443679+YSD98@users.noreply.github.com>
2022-05-08 19:49:18 +0530

    UI Button Changes

--- Assets/Scripts/UI/MenuScreen.cs
+++ Assets/Scripts/UI/MenuScreen.cs
@@ -4,19 +4,20 @@ public class MenuScreen : MonoBehaviour
 {
     public RectTransform[] Btn;
     // ----------Functions---------------------------\\
-    public void Name(){}
-    public void Play(){SceneManager.LoadScene(1);}
-    public void About()
+    public void Play(){SceneManager.LoadScene(1);}              //PLAY
+    public void About()                                         //ABOUT
     {
         for(int i=0;i<4;i++)
             Btn[i].gameObject.SetActive(false);
         Btn[4].gameObject.SetActive(true);
     }
-    public void AboutBack()
+    public void AboutBack()                                     //ABOUT_BACK
     {
         for(int i=0;i<4;i++)
             Btn[i].gameObject.SetActive(true);
         Btn[4].gameObject.SetActive(false);
     }
-    public void Exit(){Application.Quit(0);}
+    public void Exit(){Application.Quit(0);}                    //EXIT
+    public void SoundBTN(){}                                    //SOUND_BUTTON
+    public void MenuBTN(){}                                     //MENU_BUTTON
 }
